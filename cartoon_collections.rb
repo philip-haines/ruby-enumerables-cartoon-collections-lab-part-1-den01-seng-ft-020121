32980bb6bfd129c40bf7a57657c322b55d2a92c9
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-29 23:22:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |char, index|
-    puts "#{char} #{index + 1}"
+    puts {char, index + 1}
   end
 end
