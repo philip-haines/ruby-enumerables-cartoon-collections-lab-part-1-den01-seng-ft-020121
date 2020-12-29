4fefdca41664a53366b9f4b407af62d4915d5231
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-29 23:16:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,11 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  # 
+  # Print a custom greeting for each element]
+  
+  array.each do |char|
+    puts "Hello ${char}"
+  end
 end
 
 def list_dwarves(array)
